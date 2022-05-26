5470f57f3d37a14e8e74840b4832f8e0e70ae71e
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-26 19:37:36 +0530

    Add to Validations for all DTO

--- src/main/java/com/example/addressbook/dto/ContactDTO.java
+++ src/main/java/com/example/addressbook/dto/ContactDTO.java
@@ -37,17 +37,4 @@ public @Data class ContactDTO {
     @NotEmpty(message = "Email cannot be null")
     public String email;
 
-
-    public ContactDTO(String firstName, String lastName, String address, String city, String state, String zip,
-                      String phone , String email) {
-        super();
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phone = phone;
-        this.email= email;
-    }
 }
