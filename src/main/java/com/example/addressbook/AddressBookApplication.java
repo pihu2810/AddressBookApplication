11a0f1368204706d72d54b0fe541c2ca04088e3a
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-26 11:21:42 +0530

    Add to Database setting Environment Variable to AddressBook Data

--- src/main/java/com/example/addressbook/AddressBookApplication.java
+++ src/main/java/com/example/addressbook/AddressBookApplication.java
@@ -3,6 +3,7 @@ package com.example.addressbook;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
@@ -10,8 +11,11 @@ public class AddressBookApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(AddressBookApplication.class, args);
-        log.info("AddressBook App Started");
+        ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+        log.info("AddressBook App Started in {} Environment",
+                context.getEnvironment().getProperty("environment"));
+        log.info("AddressBook App DB User is {}",
+                context.getEnvironment().getProperty("spring.datasource.username"));
         System.out.println("Welcome to AddressBookApp");
     }
 
